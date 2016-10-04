8d97c812fee63b49e9f0919b8dfad56a0d5c5648
mikaelma <mikaelma@student.hist.no>
2016-10-04 17:16:29 +0200

    Sondre sine endringer

--- src/main/java/com/enmaka/matistikk/controllers/StudentController.java
+++ src/main/java/com/enmaka/matistikk/controllers/StudentController.java
@@ -21,105 +21,106 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  *
  * @author Team ENMAKA
- * 
+ *
  * Klassen har metoder for å håndtere kall fra brukertypen Student.
- * 
+ *
  * For mer informasjon om klassen, se designdokumentet kapittel 4.4.4.
  */
-
 @Controller
 public class StudentController {
-    
+
     @Autowired
     private UserService userService;
     private Test test = new Test();
-    
+
     @RequestMapping(value = "testview", method = RequestMethod.GET)
     public String testView(@ModelAttribute TestFormBackingBean backingBean, HttpSession session) {
-        if(!LoginController.validate(session, "Student")) {
+        if (!LoginController.validate(session, "Student")) {
             return "redirect:/";
         }
-        String email = ((Student)session.getAttribute("user")).getUsername();
-        int classId = ((Student)session.getAttribute("user")).getClassId();
+        String email = ((Student) session.getAttribute("user")).getUsername();
+        int classId = ((Student) session.getAttribute("user")).getClassId();
         backingBean.setAllTestableTests(userService.getAllTests(email, classId));
         backingBean.setAllPractiseTests(userService.getAllPractiseTests(email, classId));
         return "tests";
     }
-    
+
     @RequestMapping(value = "gettest")
     public String startTestView(HttpSession session, HttpServletRequest request, Model model) {
-        if(!LoginController.validate(session, "Student")) {
+        if (!LoginController.validate(session, "Student")) {
             return "redirect:/";
         }
         String s = request.getParameter("chosenTest");
-        if(s == null){
+        if (s == null) {
             return "redirect:/";
         }
         test = userService.getTest(Integer.parseInt(s), ((Student) session.getAttribute("user")).getUsername());
         model.addAttribute("test", test);
         test.getCurrentTask().startTime();
-        if(test.getCurrentTask() instanceof Arithmetic){
+        if (test.getCurrentTask() instanceof Arithmetic) {
             return "arithmetictask";
-        }else if(test.getCurrentTask() instanceof SingleChoice){
+        } else if (test.getCurrentTask() instanceof SingleChoice) {
             return "singlechoicetask";
-        }else if(test.getCurrentTask() instanceof Sort){
+        } else if (test.getCurrentTask() instanceof Sort) {
             return "sorttask";
-        }else if(test.getCurrentTask() instanceof NumberLine){
+        } else if (test.getCurrentTask() instanceof NumberLine) {
             return "numberlinetask";
-        }else if(test.getCurrentTask() instanceof Figures){
+        } else if (test.getCurrentTask() instanceof Figures) {
             return "figurestask";
-        }else{
+        } else if (test.getCurrentTask() instanceof Function) {
+            return "functiontask";
+        } else {
             return "tests";
         }
     }
-    
+
     @RequestMapping(value = "joinclass")
     public String joinClass(@ModelAttribute ClassInfo chosenClass, HttpSession session, Model model) {
-        if(!LoginController.validate(session, "Student")) {
+        if (!LoginController.validate(session, "Student")) {
             return "redirect:/";
         }
         boolean b = userService.checkClass(chosenClass.getClassId());
-        if(b == false) {
+        if (b == false) {
             String message = "ERROR: Klassen eksisterer ikke!";
             model.addAttribute("message", message);
         } else {
-            userService.updateClassId(chosenClass.getClassId(), ((User)session.getAttribute("user")).getUsername());
+            userService.updateClassId(chosenClass.getClassId(), ((User) session.getAttribute("user")).getUsername());
             chosenClass = userService.getSchoolClass(chosenClass.getClassId());
-            
-            String username = ((User)session.getAttribute("user")).getUsername();
+
+            String username = ((User) session.getAttribute("user")).getUsername();
             String testCompleteCount = userService.getTestCount(username, true, true);
             String testIncompleteCount = userService.getTestCount(username, false, true);
             String practiseTestCompleteCount = userService.getTestCount(username, true, false);
             String practiseTestIncompleteCount = userService.getTestCount(username, false, false);
-            
-            String message = "Du er nå lagt til i " + chosenClass.getSchoolClass();  
+
+            String message = "Du er nå lagt til i " + chosenClass.getSchoolClass();
             model.addAttribute("message", message);
             model.addAttribute("testCompleteCount", testCompleteCount);
             model.addAttribute("testIncompleteCount", testIncompleteCount);
             model.addAttribute("practiseTestCompleteCount", practiseTestCompleteCount);
             model.addAttribute("practiseTestIncompleteCount", practiseTestIncompleteCount);
-            ((Student)session.getAttribute("user")).setClassId(chosenClass.getClassId());
-        }        
+            ((Student) session.getAttribute("user")).setClassId(chosenClass.getClassId());
+        }
         return "mypage";
     }
-    
+
     @RequestMapping(value = "nexttask")
-    public String nextTaskView(@RequestParam String button, HttpSession session, HttpServletRequest request, Model model){
+    public String nextTaskView(@RequestParam String button, HttpSession session, HttpServletRequest request, Model model) {
         test.getCurrentTask().endTime();
-        if(!LoginController.validate(session, "Student")){
+        if (!LoginController.validate(session, "Student")) {
             return "redirect:/";
         }
         String c = request.getParameter("drawCords");
         StringTokenizer st = new StringTokenizer(c, ",");
         List<Double> cords = new ArrayList<>();
-        while(st.hasMoreTokens()){
+        while (st.hasMoreTokens()) {
             cords.add(Double.parseDouble(st.nextToken()));
         }
         String s = request.getParameter("answer");
         String d = request.getParameter("description");
-        if(button.equals("previous")){
+        if (button.equals("previous")) {
             test.previousTask();
-        }else if(test.getCurrentTask() instanceof Arithmetic || test.getCurrentTask() instanceof SingleChoice || test.getCurrentTask() instanceof NumberLine){
+        } else if (test.getCurrentTask() instanceof Arithmetic || test.getCurrentTask() instanceof SingleChoice || test.getCurrentTask() instanceof NumberLine) {
             st = new StringTokenizer(s, "/");
             int num = Integer.parseInt(st.nextToken());
             int den = Integer.parseInt(st.nextToken());
@@ -127,11 +128,11 @@ public class StudentController {
             asf.setCoordinates(cords);
             asf.setTime(test.getCurrentTask().getTime());
             test.setAnswer(asf);
-        }else if(test.getCurrentTask() instanceof Sort){
+        } else if (test.getCurrentTask() instanceof Sort) {
             st = new StringTokenizer(s, "|");
             System.out.println(s);
             ArrayList<Fraction> fr = new ArrayList<>();
-            while(st.hasMoreTokens()) {
+            while (st.hasMoreTokens()) {
                 StringTokenizer st2 = new StringTokenizer(st.nextToken(), "/");
                 int num = Integer.parseInt(st2.nextToken());
                 int den = Integer.parseInt(st2.nextToken());
@@ -143,34 +144,36 @@ public class StudentController {
             amf.setCoordinates(cords);
             amf.setTime(test.getCurrentTask().getTime());
             test.setAnswer(amf);
-        }else if(test.getCurrentTask() instanceof Figures){
+        } else if (test.getCurrentTask() instanceof Figures || test.getCurrentTask() instanceof Function) {
             Answer as = new AnswerString(d, ((Student) session.getAttribute("user")).getUsername(), (Integer) test.getCurrentTask().getId(), s);
             as.setCoordinates(cords);
             as.setTime(test.getCurrentTask().getTime());
             test.setAnswer(as);
         }
-        if(button.equals("next")){
+        if (button.equals("next")) {
             userService.addAnswer(test);
             test.setStarted(true);
-            if(test.getCounter() < test.getLength()-1){
+            if (test.getCounter() < test.getLength() - 1) {
                 test.nextTask();
-            }else{
+            } else {
                 return "tests";
             }
         }
         model.addAttribute("test", test);
         test.getCurrentTask().startTime();
-        if(test.getCurrentTask() instanceof Arithmetic){
+        if (test.getCurrentTask() instanceof Arithmetic) {
             return "arithmetictask";
-        }else if(test.getCurrentTask() instanceof SingleChoice){
+        } else if (test.getCurrentTask() instanceof SingleChoice) {
             return "singlechoicetask";
-        }else if(test.getCurrentTask() instanceof Sort){
+        } else if (test.getCurrentTask() instanceof Sort) {
             return "sorttask";
-        }else if(test.getCurrentTask() instanceof NumberLine){
+        } else if (test.getCurrentTask() instanceof NumberLine) {
             return "numberlinetask";
-        }else if(test.getCurrentTask() instanceof Figures){
+        } else if (test.getCurrentTask() instanceof Figures) {
             return "figurestask";
+        } else if (test.getCurrentTask() instanceof Function) {
+            return "functiontask";
         }
         return "tests";
-    }    
+    }
 }
